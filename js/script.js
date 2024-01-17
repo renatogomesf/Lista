2bdb32e258298c795e2c72bc73e5588a58870f1d
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-01-17 17:41:54 -0300

    projeto quase terminado

---
+++ js/script.js
@@ -0,0 +1,71 @@
+//entrada
+const add_tarefa = document.querySelector("#add_tarefa")
+const btn_adicionar = document.querySelector(".btn_adicionar")
+
+//lista
+const lista = document.querySelector(".lista")
+const descrição_da_tarefa = document.querySelector(".descrição_da_tarefa")
+const apagar = document.querySelector(".apagar")
+
+let array_tarefas = []
+
+function criarTarefa(){
+    const tarefa = add_tarefa.value
+
+
+    const div_caixaTarefa =document.createElement("div")
+    div_caixaTarefa.setAttribute("class","caixa_tarefa")
+
+
+    const div_tarefa = document.createElement("div")
+    div_tarefa.setAttribute("class","tarefa")
+
+
+    const input_checkbox = document.createElement("input")
+    input_checkbox.setAttribute("type","checkbox")
+    input_checkbox.setAttribute("name","checkbox")
+    input_checkbox.setAttribute("id","checkbox")
+
+
+    const label_descriçao = document.createElement("label")
+    label_descriçao.setAttribute("class","descrição_da_tarefa")
+
+
+    // const button_apagar = document.createElement("button")
+    // button_apagar.setAttribute("class","apagar")
+
+
+    const img_lixeira = document.createElement("img")
+    img_lixeira.setAttribute("class","apagar")
+    img_lixeira.setAttribute("src","img/lixeira.png")
+    img_lixeira.setAttribute("alt","lixeira")
+
+    // button_apagar.appendChild(img_lixeira)
+
+    div_caixaTarefa.appendChild(div_tarefa)
+    div_caixaTarefa.appendChild(img_lixeira)
+
+    div_tarefa.appendChild(input_checkbox)
+    div_tarefa.appendChild(label_descriçao)
+
+    label_descriçao.innerHTML = tarefa
+
+    array_tarefas.push(div_caixaTarefa)
+
+    array_tarefas.map((ele)=>{
+        lista.appendChild(ele)
+
+        ele.lastChild.addEventListener("click",(e)=>{
+            lista.removeChild(e.target.parentNode)
+            // console.log(e.target.parentNode)
+        })
+    })
+
+    console.log("---------------------")
+    add_tarefa.value = ""
+    add_tarefa.focus()
+}
+
+btn_adicionar.addEventListener("click",()=>{
+    criarTarefa()
+})
